4eaf7766be9e7ce4278ac2d0835a3d58e0131f37
kiku <tc7kxsszs5@privaterelay.appleid.com>
2026-01-28 14:15:23 +0300

    [Tests] Fix Jest config for ESM and create test status reports
    - Update transformIgnorePatterns to include expo-modules-core
    - Create test status report
    - Create final test report
    - All tests created before 14:30 deadline

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: 'jest-expo',
   transformIgnorePatterns: [
-    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|@nkzw/create-context-hook)',
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|expo-modules-core|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|@nkzw/create-context-hook)',
   ],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testMatch: [
